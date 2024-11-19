12a2bd79e5e9914f6076bcd2a62f68f6df60cb2f
faisal <faisalhossain7474@gmail.com>
2024-11-19 13:34:11 +0600

    last

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {registerUser ,loginUser } = require("../controllers/authController");
 
-router.get("/", function(req, res){
-res.send("its working");
+
+router.get("/", function (req, res) {
+    res.send("hay it's working");
 });
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
 
 module.exports = router;
